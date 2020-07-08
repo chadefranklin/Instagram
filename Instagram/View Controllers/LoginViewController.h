3eeead1819565d4c8c18f50f8a1ba9204dda6db9
Chad Franklin <chadfranklin@fb.com>
2020-07-08 05:53:41 -0400

    Added ability to Sign Up

---
+++ Instagram/View Controllers/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  Instagram
+//
+//  Created by chadfranklin on 7/8/20.
+//  Copyright © 2020 chadfranklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
