af4a98c19c4db05d3b70524dd9ed396c0a07f970
Chad Franklin <chadfranklin@fb.com>
2020-07-09 09:41:38 -0400

    Added Feed View Controller with Ability to Post & View Posts

---
+++ Instagram/View Controllers/FeedViewController.m
@@ -0,0 +1,82 @@
+//
+//  FeedViewController.m
+//  Instagram
+//
+//  Created by chadfranklin on 7/8/20.
+//  Copyright © 2020 chadfranklin. All rights reserved.
+//
+
+#import "FeedViewController.h"
+#import <Parse/Parse.h>
+#import "PostCell.h"
+#import "Post.h"
+
+@interface FeedViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *feedTableView;
+
+@property (nonatomic, strong) NSArray *posts;
+@end
+
+@implementation FeedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.feedTableView.dataSource = self;
+    self.feedTableView.delegate = self;
+    
+    [self fetchFeed];
+}
+
+- (void)fetchFeed{
+    // construct PFQuery
+    PFQuery *postQuery = [Post query];
+    [postQuery orderByDescending:@"createdAt"];
+    [postQuery includeKey:@"author"];
+    postQuery.limit = 20;
+
+    // fetch data asynchronously
+    [postQuery findObjectsInBackgroundWithBlock:^(NSArray<Post *> * _Nullable posts, NSError * _Nullable error) {
+        if (posts) {
+            // do something with the data fetched
+            self.posts = posts;
+            [self.feedTableView reloadData];
+        }
+        else {
+            // handle error
+        }
+    }];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    //return 20;
+    return self.posts.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    PostCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PostCell"];
+    
+    PFObject *post = self.posts[indexPath.row];
+    
+    [cell setPost:post];
+        
+    return cell;
+}
+
+- (IBAction)onComposePressed:(id)sender {
+    
+    [self performSegueWithIdentifier:@"composeSegue" sender:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
