a57c1150067c59dc50c90fdaa34b293c508bb9c4
Chad Franklin <chadfranklin@fb.com>
2020-07-11 02:18:37 -0400

    Added ability to see post and it's comments in the detailed view

--- Instagram/View Controllers/PostDetailsViewController.m
+++ Instagram/View Controllers/PostDetailsViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "PostDetailsViewController.h"
+#import "Post.h"
+#import "PostCell.h"
+#import "ComposeCommentCell.h"
+#import "CommentCell.h"
+#import "Comment.h"
+#import <Parse/Parse.h>
 
-@interface PostDetailsViewController () <UITableViewDelegate, UITableViewDataSource>
-
+@interface PostDetailsViewController () <UITableViewDelegate, UITableViewDataSource, ComposeCommentCellDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *detailsTableView; // contains post and comments
+@property (nonatomic, strong) NSMutableArray *comments;
 @end
 
 @implementation PostDetailsViewController
@@ -17,6 +24,79 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.detailsTableView.dataSource = self;
+    self.detailsTableView.delegate = self;
+    
+    PFQuery *postQuery = [Comment query];
+    [postQuery orderByDescending:@"createdAt"];
+    //[postQuery includeKey:@"createdAt"]; // don't think this is necessary
+    [postQuery includeKey:@"author"];
+    postQuery.limit = 20;
+
+    // fetch data asynchronously
+    [postQuery findObjectsInBackgroundWithBlock:^(NSArray<Comment *> * _Nullable comments, NSError * _Nullable error) {
+        if (comments) {
+            // do something with the data fetched
+            self.comments = comments;
+            [self.detailsTableView reloadData];
+        }
+        else {
+            // handle error
+        }
+        
+        //[self.refreshControl endRefreshing];
+    }];
+    
+    NSLog(@"details view did load");
+    [self.detailsTableView reloadData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    //return self.posts.count;
+    return 2 + self.comments.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(indexPath.row == 0){
+        NSLog(@"Details Post Cell");
+        PostCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DetailsPostCell"];
+        
+        //PFObject *post = self.posts[indexPath.row];
+        
+        [cell setPost:self.post];
+            
+        NSLog(@"%d", indexPath.row);
+        return cell;
+    }
+    else if(indexPath.row == 1){
+        ComposeCommentCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ComposeCommentCell"];
+        
+        //PFObject *post = self.posts[indexPath.row];
+        
+        [cell setPost:self.post];
+        
+        cell.delegate = self;
+            
+        return cell;
+    }
+    else{
+        CommentCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CommentCell"];
+        
+        [cell setComment:self.comments[indexPath.row - 2]];
+        return cell;
+    }
+}
+
+- (void)didPostComment:(NSString *)comment{
+    NSLog(@"did post comment");
+    
+    Comment *newComment = [Comment new];
+    newComment.author = [PFUser currentUser];
+    newComment.comment = comment;
+    
+    [self.comments insertObject:newComment atIndex:0];
+    [self.detailsTableView reloadData];
 }
 
 /*
